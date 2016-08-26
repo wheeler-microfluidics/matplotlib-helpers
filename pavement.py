221cf13bc8649fe64c0b07cae033115fdea17c13
Christian Fobel <christian@fobel.net>
2016-08-26 06:46:03 -0400

    Initial commit

---
+++ pavement.py
@@ -0,0 +1,29 @@
+import sys
+
+from paver.easy import task, needs, path, sh, cmdopts, options
+from paver.setuputils import setup, install_distutils_tasks
+from distutils.extension import Extension
+from distutils.dep_util import newer
+
+sys.path.insert(0, path('.').abspath())
+import version
+
+setup(name='matplotlib-helpers',
+      version=version.getVersion(),
+      description='Add description here.',
+      keywords='',
+      author='Anonymous',
+      author_email='you@mail.com',
+      url='https://github.com/wheeler-microfluidics/matplotlib-helpers',
+      license='GPL',
+      packages=['matplotlib_helpers', ],
+      install_requires=[],
+      # Install data listed in `MANIFEST.in`
+      include_package_data=True)
+
+
+@task
+@needs('generate_setup', 'minilib', 'setuptools.command.sdist') 
+def sdist():
+    """Overrides sdist to make sure that our setup.py is generated."""
+    pass
